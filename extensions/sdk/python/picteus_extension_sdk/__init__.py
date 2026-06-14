78f4338ced8dc9014d7e9c8297d7e7c273bd1641
edouardmercier <register@edouardmercier.fr>
2026-06-14 11:40:07 +0200

    #1 #time 5m Upgrades to the latest version of the SDK.

--- extensions/sdk/python/picteus_extension_sdk/__init__.py
+++ extensions/sdk/python/picteus_extension_sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__: str = "0.11.0"
+__version__: str = "0.12.0"
 
 
 def get_version() -> str:
--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,7 +2,7 @@ export const product =
   {
     name: "Picteus",
     technicalId: "picteus",
-    applicationVersion: "0.8.0",
+    applicationVersion: "0.9.0",
     apiVersion: "0.16.0",
     author:
       {
