02a6f7011bc53d9e1c5b48614c512b5b66219439
edouardmercier <register@edouardmercier.fr>
2026-06-30 22:53:49 +0200

    #2 #time 5m Attempts to get rid of a GitHub Actions CI / CD issue with the "1mwebservices.test.ts" automated test.

--- back-end/tests/jest.config.ts
+++ back-end/tests/jest.config.ts
@@ -1,18 +1,23 @@
 import fs from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 
 import type { Config } from "@jest/types";
 import { createDefaultEsmPreset, pathsToModuleNameMapper } from "ts-jest";
 
 
-const rootDirectoryPath = path.join("..");
-const tsConfigJsonFilePath = path.join(rootDirectoryPath, "tsconfig.json");
+const currentDirectoryPath = path.dirname(fileURLToPath(import.meta.url));
+const rootDirectoryPath = path.resolve(currentDirectoryPath, "..");
+const stConfigFileName = "tsconfig.json";
+const tsConfigJsonFilePath = path.join(rootDirectoryPath, stConfigFileName);
+const testTsConfigJsonFilePath = path.join(currentDirectoryPath, stConfigFileName);
 const tsConfigJson = JSON.parse(fs.readFileSync(tsConfigJsonFilePath, { encoding: "utf8" }));
 
 // This work-around comes from https://github.com/kulshekhar/ts-jest/issues/414 and at https://stackoverflow.com/questions/52860868/typescript-paths-not-resolving-when-running-jest
 const moduleNameMapper = pathsToModuleNameMapper(tsConfigJson.compilerOptions.paths, { prefix: "<rootDir>/" });
 export default {
-  ...createDefaultEsmPreset({ tsconfig: tsConfigJsonFilePath }),
+  ...createDefaultEsmPreset({ tsconfig: testTsConfigJsonFilePath }),
   testEnvironment: "node",
   rootDir: rootDirectoryPath,
   // The mappings after the first one are there to solve an issue caused by the Prisma generated client "client.ts" class code, which resorts to '.js' extensions in its import statements, and the work-around is taken from https://github.com/kulshekhar/ts-jest/issues/1057. Unfortunately, we cannot apply the  "(.+)\\.js": "$1" mapping, because this breaks other things
