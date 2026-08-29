d3e2f841db3ee1ad7b161082f1462010de61923b
edouardmercier <register@edouardmercier.fr>
2026-08-29 17:46:04 +0200

    #2 #time 5m Fixes a TypeScript compilation issue in the "esxample-typescript" extension.

--- extensions/instances/example-typescript/src/main.ts
+++ extensions/instances/example-typescript/src/main.ts
@@ -2,7 +2,7 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 
 import {
-  CommandParameters,
+  type CommandParameters,
   Communicator,
   ImageFeatureFormat,
   ImageFeatureType,
@@ -13,7 +13,7 @@ import {
   type IntentImage,
   IntentShowType,
   IntentUiAnchor,
-  OpenBrowserIntent,
+  type OpenBrowserIntent,
   PicteusExtension,
   type ProcessCommandIntent,
   type SettingsValue,
